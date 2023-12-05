eb4d88850d98fedaa8e692ab03d04bb12d94cfbb
pnocera <pnocera@gmail.com>
2023-12-05 06:15:56 +0100

    otw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,13 @@ import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 })
 export class AppComponent {
   validateForm: FormGroup<{
-    email: FormControl<string>;
-    password: FormControl<string>;
-    checkPassword: FormControl<string>;
-    nickname: FormControl<string>;
-    phoneNumberPrefix: FormControl<'+86' | '+87'>;
-    phoneNumber: FormControl<string>;
-    website: FormControl<string>;
-    captcha: FormControl<string>;
-    agree: FormControl<boolean>;
+    entity: FormControl<string>;
+    project: FormControl<string>;
+    riskassesment: FormControl<string>;
+    address: FormControl<string>;
+    date: FormControl<Date | null>;
+    supervisor: FormControl<string>;
+    dbyd: FormControl<string>;
   }>;
   captchaTooltipIcon: NzFormTooltipIcon = {
     type: 'info-circle',
@@ -33,7 +31,7 @@ export class AppComponent {
 
   submitForm(): void {
     if (this.validateForm.valid) {
-      console.log('submit', this.validateForm.value);
+      alert('Saved ' + JSON.stringify(this.validateForm.value));
     } else {
       Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
@@ -44,23 +42,23 @@ export class AppComponent {
     }
   }
 
-  updateConfirmValidator(): void {
-    /** wait for refresh value */
-    Promise.resolve().then(() =>
-      this.validateForm.controls.checkPassword.updateValueAndValidity()
-    );
-  }
+  // updateConfirmValidator(): void {
+  //   /** wait for refresh value */
+  //   Promise.resolve().then(() =>
+  //     this.validateForm.controls.checkPassword.updateValueAndValidity()
+  //   );
+  // }
 
-  confirmationValidator: ValidatorFn = (
-    control: AbstractControl
-  ): { [s: string]: boolean } => {
-    if (!control.value) {
-      return { required: true };
-    } else if (control.value !== this.validateForm.controls.password.value) {
-      return { confirm: true, error: true };
-    }
-    return {};
-  };
+  // confirmationValidator: ValidatorFn = (
+  //   control: AbstractControl
+  // ): { [s: string]: boolean } => {
+  //   if (!control.value) {
+  //     return { required: true };
+  //   } else if (control.value !== this.validateForm.controls.password.value) {
+  //     return { confirm: true, error: true };
+  //   }
+  //   return {};
+  // };
 
   getCaptcha(e: MouseEvent): void {
     e.preventDefault();
@@ -68,15 +66,13 @@ export class AppComponent {
 
   constructor(private fb: NonNullableFormBuilder) {
     this.validateForm = this.fb.group({
-      email: ['', [Validators.email, Validators.required]],
-      password: ['', [Validators.required]],
-      checkPassword: ['', [Validators.required, this.confirmationValidator]],
-      nickname: ['', [Validators.required]],
-      phoneNumberPrefix: '+86' as '+86' | '+87',
-      phoneNumber: ['', [Validators.required]],
-      website: ['', [Validators.required]],
-      captcha: ['', [Validators.required]],
-      agree: [false],
+      entity: ['', [Validators.required]],
+      project: ['', [Validators.required]],
+      riskassesment: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      date: this.fb.control<Date | null>(new Date()),
+      supervisor: ['', []],
+      dbyd: ['', []],
     });
   }
 }
